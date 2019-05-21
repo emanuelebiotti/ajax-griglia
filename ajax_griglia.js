f2dbc487c9d4922c0e957c12d365659e3b8b09a0
emanuele <emanuele.biotti@gmail.com>
2019-05-21 17:08:40 +0200

    I quadrati cambiano colore al ckick a seconda del numero chiamato

--- ajax_griglia.js
+++ ajax_griglia.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
 
-  $.ajax({
-    'url': 'https://www.boolean.careers/api/random/int',
-    'method': 'GET',
-    'success': function(data){
-      console.log(data.response);
-    },
-    'error': function() {
-      alert('errore');
-    }
-  });
+  // $.ajax({
+  //   'url': 'https://www.boolean.careers/api/random/int',
+  //   'method': 'GET',
+  //   'success': function(data){
+  //     console.log(data.response);
+  //   },
+  //   'error': function() {
+  //     alert('errore');
+  //   }
+  // });
 
 
 
@@ -22,9 +22,15 @@ $('.quadrato').click(function(){
       'method': 'GET',
       'success': function(data){
         questoquadrato.text(data.response);
-        
-
+        var numero = data.response;
+        console.log(numero);
+        if(numero <= 5) {
+          questoquadrato.css('backgroundColor', 'yellow');
+        } else {
+          questoquadrato.css('backgroundColor', 'green')
+        }
       },
+
       'error': function() {
         alert('errore');
       }
