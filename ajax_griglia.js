bad35151e905edc254044e76d60e66e1b5873495
emanuele <emanuele.biotti@gmail.com>
2019-05-31 15:19:56 +0200

    Aggiunta controllo su click

--- ajax_griglia.js
+++ ajax_griglia.js
@@ -1,34 +1,41 @@
 $(document).ready(function(){
 
-var source   = document.getElementById("entry-template").innerHTML;
-var template = Handlebars.compile(source);
+  var source   = document.getElementById("entry-template").innerHTML;
+  var template = Handlebars.compile(source);
 
-for (i = 0; i<36; i++){
-  var context = {};
-  var html = template(context);
-  $('.container').append(html)
-}
+  for (i = 0; i<36; i++){
+    var context = {};
+    var html = template(context);
+    $('.container').append(html)
+  }
+
+  var cliccati = [];
+
   $('.quadratino').click(function(){
   var questoquadrato = $(this);
 
-    $.ajax({
-      'url': 'https://www.boolean.careers/api/random/int',
-      'method': 'GET',
-      'success': function(numeri){
-        console.log(numeri.response);
+  if (!cliccati.includes(questoquadrato.index())) {
 
-        questoquadrato.text(numeri.response);
-        if (numeri.response <=5) {
-          questoquadrato.css('background-color', 'yellow');
-        } else {
-          questoquadrato.css('background-color', 'green')
+      $.ajax({
+        'url': 'https://www.boolean.careers/api/random/int',
+        'method': 'GET',
+        'success': function(numeri){
+
+          questoquadrato.text(numeri.response);
+
+            if (numeri.response <=5) {
+              questoquadrato.css('background-color', 'yellow');
+            } else {
+              questoquadrato.css('background-color', 'green')
+            }
+
+          cliccati.push(questoquadrato.index())
+        },
+        'error': function(){
+          alert('errore');
         }
-      },
-      'error': function(){
-        alert('errore');
-      }
-    });
-
+      });
+    }
   })
 
 
