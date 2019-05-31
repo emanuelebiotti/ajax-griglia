6e8820b11f0ff55e35f155689cac8e8383c02af6
emanuele <emanuele.biotti@gmail.com>
2019-05-31 09:22:58 +0200

    Nuovo codice

--- ajax_griglia.js
+++ ajax_griglia.js
@@ -1,44 +1,26 @@
 $(document).ready(function(){
 
-  // $.ajax({
-  //   'url': 'https://www.boolean.careers/api/random/int',
-  //   'method': 'GET',
-  //   'success': function(data){
-  //     console.log(data.response);
-  //   },
-  //   'error': function() {
-  //     alert('errore');
-  //   }
-  // });
-
-
-
-$('.quadrato').click(function(){
+  $('.quadratino').click(function(){
   var questoquadrato = $(this);
-  console.log(questoquadrato);
 
     $.ajax({
       'url': 'https://www.boolean.careers/api/random/int',
       'method': 'GET',
-      'success': function(data){
-        questoquadrato.text(data.response);
-        var numero = data.response;
-        console.log(numero);
-        if(numero <= 5) {
-          questoquadrato.css('backgroundColor', 'yellow');
+      'success': function(numeri){
+        console.log(numeri.response);
+        questoquadrato.text(numeri.response);
+        if (numeri.response <=5) {
+          questoquadrato.css('background-color', 'yellow');
         } else {
-          questoquadrato.css('backgroundColor', 'green')
+          questoquadrato.css('background-color', 'green')
         }
       },
-
-      'error': function() {
+      'error': function(){
         alert('errore');
       }
-    })
+    });
 
-
-  });
-
+  })
 
 
 });
